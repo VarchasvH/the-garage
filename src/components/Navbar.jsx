248c3c6739b0961f8082d552f045aa909fbe0633
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-05-12 10:31:17 +0530

    navbar done, moving on to collection area

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import india from "../../public/images/india-flag.svg";
 import phoneIcon from "../../public/images/phone-icon.svg";
 import homeIcon from "../../public/images/home-icon.svg";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-      <nav className=' bg-[#0d0d0d] bg-opacity-70 py-3 flex justify-between items-center px-28 font-serif font-light'>
+      <nav className=' bg-[#0d0d0d] bg-opacity-60 py-3 flex justify-between items-center px-28 font-serif font-light'>
         <div className='flex items-center text-sm'>
           <div className='flex gap-4 '>
             <Image src={phoneIcon} alt='Phone Icon' width={25} height={25} />
@@ -24,7 +25,9 @@ const Navbar = () => {
             <p className='font-semibold mt-1 ml-3.5'>New Delhi, India</p>
           </div>
         </div>
-        <div className=' text-3xl mr-60'>The Garage</div>
+        <Link href='/'>
+          <div className=' text-3xl mr-60'>The Garage</div>
+        </Link>
         <Image
           src={india}
           alt='India Flag'
